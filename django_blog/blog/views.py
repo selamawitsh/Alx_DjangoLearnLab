f3b17b2f70e98dab71b601bb540edd493f729d06
Selamawit Shimeles <selamshimeles9@gmail.com>
2025-12-06 18:18:26 +0300

    task 1 week 14

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm
+from django.contrib import messages
 
-# Create your views here.
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, "Account created successfully!")
+            return redirect('login')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'blog/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('profile')
+        else:
+            messages.error(request, "Invalid username or password")
+
+    return render(request, 'blog/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required
+def profile_view(request):
+    return render(request, 'blog/profile.html')
+
+
+
+@login_required
+def profile_update_view(request):
+    if request.method == "POST":
+        user = request.user
+        user.email = request.POST.get('email')
+        user.save()
+        messages.success(request, "Profile updated successfully!")
+        return redirect('profile')
+
+    return render(request, 'blog/profile_update.html')
